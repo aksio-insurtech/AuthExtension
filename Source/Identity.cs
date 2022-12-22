90592f8878341c7b299de8d856cf634e78ed4a98
Einar Ingebrigtsen <einari@me.com>
2022-12-22 13:23:30 +0100

    Make content of identity cookie be base64 encoded string

--- Source/Identity.cs
+++ Source/Identity.cs
@@ -1,6 +1,9 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Net;
+using System.Text;
+
 namespace Aksio.IngressMiddleware;
 
 public static class Identity
@@ -23,8 +26,14 @@ public static class Identity
                 client.DefaultRequestHeaders.Add(Headers.PrincipalId, request.Headers[Headers.PrincipalId].ToString());
                 client.DefaultRequestHeaders.Add(Headers.PrincipalName, request.Headers[Headers.PrincipalName].ToString());
                 var responseMessage = await client.GetAsync(config.IdentityDetailsUrl);
+
+                if (responseMessage.StatusCode == HttpStatusCode.Forbidden)
+                {
+                    response.StatusCode = 403;
+                }
                 var identityDetails = await responseMessage.Content.ReadAsStringAsync();
-                response.Cookies.Append(CookieName, identityDetails);
+                var identityDetailsAsBase64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(identityDetails));
+                response.Cookies.Append(CookieName, identityDetailsAsBase64);
             }
             catch (Exception ex)
             {
