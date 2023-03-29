deca3b9c6fbaa600625a27c9b8959fe7eee98c6b
Einar Ingebrigtsen <einari@me.com>
2023-03-29 16:05:14 +0200

    Forward tenant id

--- Source/Identity.cs
+++ Source/Identity.cs
@@ -31,6 +31,7 @@ public static class Identity
                 client.DefaultRequestHeaders.Add(Headers.Principal, request.Headers[Headers.Principal].ToString());
                 client.DefaultRequestHeaders.Add(Headers.PrincipalId, request.Headers[Headers.PrincipalId].ToString());
                 client.DefaultRequestHeaders.Add(Headers.PrincipalName, request.Headers[Headers.PrincipalName].ToString());
+                client.DefaultRequestHeaders.Add(Headers.TenantId, request.Headers[Headers.TenantId].ToString());
                 var responseMessage = await client.GetAsync(config.IdentityDetailsUrl);
 
                 if (responseMessage.StatusCode == HttpStatusCode.Forbidden)
