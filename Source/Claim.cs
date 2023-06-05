b76129b3007c45e02838d93a8d48fc926b84b0a0
Einar Ingebrigtsen <einari@me.com>
2023-06-05 11:59:49 +0200

    Renaming properties to make more sense

--- Source/Claim.cs
+++ Source/Claim.cs
@@ -1,6 +1,25 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Text.Json.Serialization;
+
 namespace Aksio.IngressMiddleware;
 
-public record Claim(string Typ, string Val);
+public class Claim
+{
+    public Claim()
+    {
+    }
+
+    public Claim(string type, string value)
+    {
+        Type = type;
+        Value = value;
+    }
+
+    [JsonPropertyName("typ")]
+    public string Type { get; set; } = string.Empty;
+
+    [JsonPropertyName("val")]
+    public string Value { get; set; } = string.Empty;
+}
