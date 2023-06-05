13eefe41e926bd2575006e231157b182903a4f4e
Einar Ingebrigtsen <einari@me.com>
2023-06-05 12:00:07 +0200

    Moving loading of config into the Config object

--- Source/Config.cs
+++ Source/Config.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Text.Json;
+
 namespace Aksio.IngressMiddleware;
 
 public class Config
@@ -10,4 +12,17 @@ public class Config
     public TenantResolutionConfig TenantResolution { get; set; } = new TenantResolutionConfig();
     public string IdentityDetailsUrl { get; set; } = string.Empty;
     public OAuthBearerTokensConfig OAuthBearerTokens { get; set; } = new OAuthBearerTokensConfig();
+
+    public static Config Load()
+    {
+        const string configFile = "./config/config.json";
+        var config = new Config();
+        if (File.Exists(configFile))
+        {
+            var configJson = File.ReadAllText(configFile);
+            config = JsonSerializer.Deserialize<Config>(configJson, Globals.JsonSerializerOptions)!;
+        }
+
+        return config;
+    }
 }
