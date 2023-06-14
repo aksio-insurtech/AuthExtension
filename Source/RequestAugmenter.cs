0bdaf1d4c3e4a42d1b4f5c41ce651173a4ab0055
Einar Ingebrigtsen <einari@me.com>
2023-06-14 08:04:48 +0200

    Adding header debut output

--- Source/RequestAugmenter.cs
+++ Source/RequestAugmenter.cs
@@ -48,6 +48,13 @@ public class RequestAugmenter : Controller
     [HttpGet]
     public async Task<IActionResult> Get()
     {
+        Console.WriteLine("*********** HEADERS ***********");
+        foreach (var key in Request.Headers.Keys)
+        {
+            Console.WriteLine($"{key}: {Request.Headers[key]}");
+        }
+        Console.WriteLine("*********** HEADERS ***********");
+
         var tenantId = await _tenantResolver.Resolve(Request);
         Response.Headers[Headers.TenantId] = tenantId.ToString();
 
