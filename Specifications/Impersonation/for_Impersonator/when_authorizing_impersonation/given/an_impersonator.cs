86411021f37da3dd0923244a4959a43037b41ce5
Einar Ingebrigtsen <einari@me.com>
2023-06-15 14:35:49 +0200

    Fixed specs

--- Specifications/Impersonation/for_Impersonator/when_authorizing_impersonation/given/an_impersonator.cs
+++ Specifications/Impersonation/for_Impersonator/when_authorizing_impersonation/given/an_impersonator.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Aksio Insurtech. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Aksio.IngressMiddleware.Identities;
+using Aksio.IngressMiddleware.Tenancy;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -12,14 +14,18 @@ public class an_impersonator : Specification
     protected Impersonator impersonator;
     protected Mock<IServiceProvider> service_provider;
     protected Mock<IImpersonationAuthorizer> authorizer;
+    protected Mock<ITenantResolver> tenant_resolver;
+    protected Mock<IIdentityDetailsResolver> identity_details_resolver;
 
     void Establish()
     {
         service_provider = new Mock<IServiceProvider>();
         authorizer = new();
+        tenant_resolver = new();
+        identity_details_resolver = new();
         service_provider.Setup(_ => _.GetService(IsAny<Type>())).Returns(authorizer.Object);
 
-        impersonator = new(service_provider.Object, Mock.Of<ILogger<Impersonator>>())
+        impersonator = new(service_provider.Object, identity_details_resolver.Object, tenant_resolver.Object,  Mock.Of<ILogger<Impersonator>>())
         {
             ControllerContext = new ControllerContext
             {
